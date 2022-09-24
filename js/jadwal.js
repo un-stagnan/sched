10b806965fcf6f042500af0cc77dba73b3c642a2
unstagnan <72715931+unstagnan@users.noreply.github.com>
2022-09-24 23:22:14 +0700

    Initial Commit - Jadwal Semester 5

---
+++ js/jadwal.js
@@ -0,0 +1,29 @@
+const jadwal = {
+
+  'Senin' : [
+    {'jam': '07.40 - 10.10', 'ruang' : 'F-503', 'matkul': 'PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
+    {'jam': '10.10 - 11.00', 'ruang' : 'F-503', 'matkul': 'Praktikum PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
+  ],
+
+  'Selasa' : [
+    {'jam': '07.40 - 10.10', 'ruang' : 'F-503', 'matkul': 'Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
+  ],
+
+  'Rabu' : [
+    {'jam': '10.10 - 11.00', 'ruang' : 'F-503', 'matkul': 'Praktikum Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
+  ],
+
+  'Kamis' : [
+    {'jam': '10.10 - 13.20', 'ruang' : 'D-303', 'matkul': 'Sistem Kontrol', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
+    {'jam': '09.20 - 11.50', 'ruang' : 'D-302', 'matkul': 'Pengolahan Citra Digital', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
+  ],
+
+  'Jum\'at' : [
+    {'jam': '07.40 - 10.10', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'},
+    {'jam': '10.10 - 13.20', 'ruang' : 'D-302', 'matkul': 'Elektronika Industri', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'}
+  ],
+
+  'Sabtu' : [
+    {'jam': '10.10', 'ruang' : 'F-502', 'matkul': 'Praktek Kerja Industri', 'dosen' : 'Didik Riyanto, S.T., M.Kom'}
+  ]
+};
