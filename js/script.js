e3ad1b83c5c609ff732967f031272975b730b134
John Doe <lorem@ipsum.com>
2022-10-04 20:58:10 +0700

    CSS improvement

--- js/script.js
+++ js/script.js
@@ -6,18 +6,20 @@
     const kelas = jadwal[hari];
     kelas.forEach((mk) => {
       jdwl += '      <div class="card kelas">\n'
-      jdwl += '        <div>\n'
+      jdwl += '        <div class="wrapper-tempat">\n'
       jdwl += '          <span class="jam">' + mk['jam'] +'</span>\n';
       jdwl += '          <span class="ruang">' + mk['ruang'] +'</span>\n';
       jdwl += '        </div>\n'
 
       if (mk['matkul'].slice(0,9)=='Praktikum') {
-        jdwl += '        <span class="mk-prak">' + mk['matkul'] +'</span>\n';  
+        jdwl += '        <span class="mk-prak">' + mk['matkul'] +'</span>\n';
       } else {
         jdwl += '        <span class="matkul">' + mk['matkul'] +'</span>\n';
       }
 
-      jdwl += '        <span class="dosen">' + mk['dosen'] +'</span>\n';
+      jdwl += '        <div class="wrapper-dosen">\n'
+      jdwl += '          <span class="dosen">' + mk['dosen'] +'</span>\n';
+      jdwl += '        </div>\n'
       jdwl += '      </div>\n'
     });
     jdwl += '    </div>\n'
