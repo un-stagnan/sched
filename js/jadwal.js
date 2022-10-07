f2c72930755a9bcb48fbcbf4c81283ecf8141b74
unstagnan <lucidity.reality@gmail.com>
2022-10-07 12:23:38 +0700

    update jam perkuliahan

--- js/jadwal.js
+++ js/jadwal.js
@@ -14,13 +14,13 @@ const jadwal = {
   ],
 
   'Kamis' : [
+    {'jam': '07.40 - 09.20', 'ruang' : 'D-304', 'matkul': 'Pengolahan Citra Digital', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
     {'jam': '10.10 - 13.20', 'ruang' : 'D-303', 'matkul': 'Sistem Kontrol', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'}
   ],
 
   'Jum\'at' : [
-    {'jam': '07.40 - 10.10', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'},
-    {'jam': '09.20 - 11.50', 'ruang' : 'D-304', 'matkul': 'Pengolahan Citra Digital', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
-    {'jam': '10.10 - 13.20', 'ruang' : 'D-302', 'matkul': 'Elektronika Industri', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'}
+    {'jam': '07.40 - 09.20', 'ruang' : 'D-302', 'matkul': 'Elektronika Industri', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
+    {'jam': '10.10 - 11.30', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
   ],
 
   'Sabtu' : [
