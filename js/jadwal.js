a17b9be346977b9de7e8ecd5d05e1060add381ca
unstagnan <lucidity.reality@gmail.com>
2022-10-10 18:52:00 +0700

    penyesuaian jam perkuliahan

--- js/jadwal.js
+++ js/jadwal.js
@@ -2,7 +2,7 @@ const jadwal = {
 
   'Senin' : [
     {'jam': '07.40 - 09.20', 'ruang' : 'F-503', 'matkul': 'PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
-    {'jam': '10.10 - 11.00', 'ruang' : 'F-503', 'matkul': 'Praktikum PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
+    {'jam': '10.10 - 11.50', 'ruang' : 'F-503', 'matkul': 'Praktikum PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
   ],
 
   'Selasa' : [
@@ -10,17 +10,17 @@ const jadwal = {
   ],
 
   'Rabu' : [
-    {'jam': '10.10 - 11.00', 'ruang' : 'F-503', 'matkul': 'Praktikum Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
+    {'jam': '10.10 - 11.50', 'ruang' : 'F-503', 'matkul': 'Praktikum Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
   ],
 
   'Kamis' : [
     {'jam': '07.40 - 09.20', 'ruang' : 'D-304', 'matkul': 'Pengolahan Citra Digital', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
     {'jam': '10.10 - 13.20', 'ruang' : 'D-303', 'matkul': 'Sistem Kontrol', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'}
-  ],
+  ],7
 
   'Jum\'at' : [
     {'jam': '07.40 - 09.20', 'ruang' : 'D-302', 'matkul': 'Elektronika Industri', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
-    {'jam': '10.10 - 11.30', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
+    {'jam': '10.10 - 11.50', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
   ],
 
   'Sabtu' : [
