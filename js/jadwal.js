99919370f39a6b8e58d097ee8212a71123d9e748
beowulf <beowulf@this.pc>
2023-03-05 22:42:40 +0700

    Initial commit semester 6

--- js/jadwal.js
+++ js/jadwal.js
@@ -1,29 +1,19 @@
 const jadwal = {
-
-  'Senin' : [
-    {'jam': '07.40 - 09.20', 'ruang' : 'F-503', 'matkul': 'PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
-    {'jam': '10.10 - 11.50', 'ruang' : 'F-503', 'matkul': 'Praktikum PLC', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
-  ],
-
-  'Selasa' : [
-    {'jam': '10.10 - 11.50', 'ruang' : 'F-503', 'matkul': 'Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
-  ],
-
+   
   'Rabu' : [
-    {'jam': '10.10 - 11.50', 'ruang' : 'F-503', 'matkul': 'Praktikum Mikrokontroler', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
+    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
+    {'jam': '13.20', 'ruang' : 'F-504', 'matkul': 'Praktikum Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'}
   ],
-
+  
   'Kamis' : [
-    {'jam': '07.40 - 09.20', 'ruang' : 'D-304', 'matkul': 'Pengolahan Citra Digital', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'},
-    {'jam': '10.10 - 13.20', 'ruang' : 'D-303', 'matkul': 'Sistem Kontrol', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'}
+    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Manajemen Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
+    {'jam': '07.40', 'ruang' : 'D-304', 'matkul': 'Probabilitas dan Statistik', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'},
+    {'jam': '09.20', 'ruang' : 'D-302', 'matkul': 'Dasar Teknik Elektro', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
+    {'jam': '10.10', 'ruang' : 'D-303', 'matkul': 'Medan Elektromagnetik', 'dosen' : 'Desriyanti, S.T., M.Kom'}
+    
   ],
-
+  
   'Jum\'at' : [
-    {'jam': '07.40 - 09.20', 'ruang' : 'D-302', 'matkul': 'Elektronika Industri', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
-    {'jam': '10.10 - 11.50', 'ruang' : 'D-303', 'matkul': 'Kecerdasan Buatan', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'}
-  ],
-
-  'Sabtu' : [
-    {'jam': '10.10', 'ruang' : 'F-502', 'matkul': 'Praktek Kerja Industri', 'dosen' : 'Didik Riyanto, S.T., M.Kom'}
+    {'jam': '07.40', 'ruang' : 'D-302', 'matkul': 'Sinyal dan Sistem', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
   ]
 };
