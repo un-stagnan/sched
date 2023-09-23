71cc7ed95b04f0228bb4afdcf6f456e6aaffead7
John Doe <foo@bar.com>
2023-09-23 12:43:58 +0700

    Pembarauan jadwal Kuliah - Semester 7

--- js/jadwal.js
+++ js/jadwal.js
@@ -1,19 +1,17 @@
 const jadwal = {
    
   'Rabu' : [
-    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
-    {'jam': '13.20', 'ruang' : 'F-504', 'matkul': 'Praktikum Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'}
+    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Elektronika medik<br>*', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
+    {'jam': '09.20', 'ruang' : 'D-302', 'matkul': 'Green Teknologi<br>***', 'dosen' : 'Desriyanti, S.T., M.Kom'}
   ],
   
   'Kamis' : [
-    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Manajemen Jaringan Komputer', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
-    {'jam': '07.40', 'ruang' : 'D-304', 'matkul': 'Probabilitas dan Statistik', 'dosen' : 'Rhesma Intan Vidyastari, S.T., M.T.'},
-    {'jam': '09.20', 'ruang' : 'D-302', 'matkul': 'Dasar Teknik Elektro', 'dosen' : 'Muhammad Muhsin, S.T., M.Kom'},
-    {'jam': '10.10', 'ruang' : 'D-303', 'matkul': 'Medan Elektromagnetik', 'dosen' : 'Desriyanti, S.T., M.Kom'}
-    
+    {'jam': '07.40', 'ruang' : 'F-504', 'matkul': 'Sensor, aktuator, instrumentasi dan akuisisi data<br>**', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}    
   ],
   
-  'Jum\'at' : [
-    {'jam': '07.40', 'ruang' : 'D-302', 'matkul': 'Sinyal dan Sistem', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}
+  'Tidak Terjadwal' : [
+    {'jam': '--', 'ruang' : '--', 'matkul': 'Prakerin', 'dosen' : '   '},
+    {'jam': '--', 'ruang' : '--', 'matkul': 'Seminar Proposal Skripsi', 'dosen' : '   '},
+    {'jam': '--', 'ruang' : '--', 'matkul': 'Kuliah Kerja Nyata', 'dosen' : '   '},
   ]
 };
