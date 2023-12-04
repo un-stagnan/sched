ff4e30cb57a9e4c80f3d78407c457c530e3fc7c2
John Doe <foo@bar.com>
2023-12-05 06:27:55 +0700

    penyesuaian jadwal

--- js/jadwal.js
+++ js/jadwal.js
@@ -1,17 +1,19 @@
 const jadwal = {
-   
+  'Selasa' : [
+    {'jam': '07.40', 'ruang' : 'D-304', 'matkul': 'Elektronika medik<br>*', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
+  ],
+
   'Rabu' : [
-    {'jam': '07.40', 'ruang' : 'D-303', 'matkul': 'Elektronika medik<br>*', 'dosen' : 'Edy Kurniawan, S.T., M.T.'},
-    {'jam': '09.20', 'ruang' : 'D-302', 'matkul': 'Green Teknologi<br>***', 'dosen' : 'Desriyanti, S.T., M.Kom'}
+    {'jam': '07.40', 'ruang' : 'D-304', 'matkul': 'Green Teknologi<br>***', 'dosen' : 'Desriyanti, S.T., M.Kom'}
   ],
   
   'Kamis' : [
-    {'jam': '07.40', 'ruang' : 'F-504', 'matkul': 'Sensor, aktuator, instrumentasi dan akuisisi data<br>**', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}    
+    {'jam': '07.40', 'ruang' : 'F-504', 'matkul': 'Sensor, Aktuator, Instrumentasi dan Akuisisi Data<br>**', 'dosen' : 'Jawwad Sulthon Habiby, S.T., M.T.'}    
   ],
   
   'Tidak Terjadwal' : [
-    {'jam': '--', 'ruang' : '--', 'matkul': 'Prakerin', 'dosen' : '   '},
-    {'jam': '--', 'ruang' : '--', 'matkul': 'Seminar Proposal Skripsi', 'dosen' : '   '},
-    {'jam': '--', 'ruang' : '--', 'matkul': 'Kuliah Kerja Nyata', 'dosen' : '   '},
+    {'jam': '--', 'ruang' : 'D-402', 'matkul': 'Prakerin', 'dosen' : 'Didik Riyanto, S.T., M.Kom'},
+    {'jam': '--', 'ruang' : 'D-304', 'matkul': 'Seminar Proposal Skripsi', 'dosen' : 'Didik Riyanto, S.T., M.Kom'},
+    {'jam': '--', 'ruang' : 'D-305', 'matkul': 'Kuliah Kerja Nyata', 'dosen' : 'Didik Riyanto, S.T., M.Kom'},
   ]
 };
